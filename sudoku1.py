a4ae7320fa7cbce808364db8970e3e3a8d015fdb
scott <151191354+sottp@users.noreply.github.com>
2026-02-25 01:15:19 -0600

    Add files via upload

--- sudoku1.py
+++ sudoku1.py
@@ -1,15 +1,31 @@
 import sys
 from clingo.application import Application, clingo_main
 
+import sys
+import shutil
+
+def lpCombine():
+    destination = "combine.lp"
+    append_file =  "instances/lp/ex00.lp"
+
+    if len(sys.argv) == 2:
+        append_file = sys.argv[1]
+
+    open(destination, "w").close()#wipe
+
+    shutil.copyfile("sudoku.lp", destination)
+    with open(append_file, "r") as f_in, open(destination, "a") as f_out:
+        f_out.write(f_in.read())
+
 
 class SudokuApp(Application):
     def main(self, ctl, files):
+        lpCombine()
+
+        ctl.load("combine.lp")
 
         for f in files:
             ctl.load(f)
-
-        if not files:
-            ctl.load("-")
 
         ctl.ground([("base", [])])
         ctl.solve()
@@ -17,8 +33,6 @@ class SudokuApp(Application):
     def print_model(self, model, printer):
         atoms = sorted(model.symbols(shown=True), key=str)
         print(" ".join(str(a) for a in atoms))
-
-
 
 if __name__ == "__main__":
     clingo_main(SudokuApp(), sys.argv[1:])
--- sudoku4.py
+++ sudoku4.py
@@ -2,15 +2,30 @@ import sys
 from clingo.application import Application, clingo_main
 from sudoku_board import Sudoku
 
+import sys
+import shutil
+
+def lpCombine():
+    destination = "combine.lp"
+    append_file = "instances/lp/ex00.lp"
+
+    if len(sys.argv) == 2:
+        append_file = sys.argv[1]
+
+    open(destination, "w").close()  # wipe
+
+    shutil.copyfile("sudoku.lp", destination)
+    with open(append_file, "r") as f_in, open(destination, "a") as f_out:
+        f_out.write(f_in.read())
 
 class SudokuApp(Application):
     def main(self, ctl, files):
+        lpCombine()
+
+        ctl.load("combine.lp")
 
         for f in files:
             ctl.load(f)
-
-        if not files:
-            ctl.load("-")
 
         ctl.ground([("base", [])])
         ctl.solve()
